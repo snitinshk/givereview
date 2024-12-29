a30cd540c5817ed53ef181fdcb4431bb5b0893a8
rajnikant.work@gmail.com <rajnikant.work@gmail.com>
2024-12-29 17:32:02 +0530

    ui fixes in widget

--- public/js/testimonials-slider.js
+++ public/js/testimonials-slider.js
@@ -1,5 +1,6 @@
 (function () {
-  const baseUrl = `https://app.givereview.to`;
+  //const baseUrl = `https://app.givereview.to`;
+  const baseUrl = `http://localhost:3000`;
 
   const scriptTag = document.querySelector('script[data-widget="testimonials-slider"]');
   const widgetId = scriptTag.getAttribute("widget-id");
@@ -79,8 +80,6 @@
           <div class="swiper-wrapper">
             ${generateSlides(externalReviews)}
           </div>
-          <div class="swiper-button-prev"></div>
-          <div class="swiper-button-next"></div>
         </div>
       </div>
       ${widget.show_powered_by ? '<footer class="plcboot-widget-copyright">Powered with ❤️ by Place Booster</footer>' : ''}
@@ -126,7 +125,7 @@
             </div>
             <img src="${review.channels.channel_logo_url}" alt="${review.channels.channel_name}" class="platform-icon" />
           </div>
-          <div class="review-rating">★${'★'.repeat(review.review_count-1)}</div>
+          <div class="review-rating">★${'★'.repeat(review.review_count - 1)}</div>
           <p class="review-text">${review.review_description}</p>
         </div>
       </div>
@@ -159,17 +158,23 @@
 
     const swiperContainer = document.querySelector(".tab-content-wrapper");
     swiperContainer.innerHTML = `
-      <div class="swiper-container" id="swiper-container-${platform}" widget-platform="${platform}">
+    <div class="sliderM_container">
+      <div class="swiper-container" id="swiper-container-${platform}" widget-platform="${platform}" css-mode="true">
         <div class="swiper-wrapper">
           ${generateSlides(filteredReviews)}
         </div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
       </div>
+      <div class="swiper-button-prev">
+          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4"><path d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+      </div>
+      <div class="swiper-button-next">
+    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+      </div>
+     </div>
     `;
 
     activeSwiper = new Swiper(`#swiper-container-${platform}`, {
-      loop: true,
+      loop: false,
       speed: 900,
 
       autoplay: {
