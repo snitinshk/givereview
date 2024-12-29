a6476095ee0de98563b44cfda8819fb433e1e32c
Shivam <mailtoshivamshk@gmail.com>
2024-12-29 11:47:17 +0530

    widget test5

--- app/api/web/widget/route.ts
+++ app/api/web/widget/route.ts
@@ -1,59 +1,38 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@/lib/supabase/supabase-server';
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
-
-export async function OPTIONS() {
-  // Handle preflight request
-  return new Response(null, {
-    status: 200,
-    headers: corsHeaders,
-  });
-}
 
 export async function GET(request: NextRequest) {
+
   try {
     const clientId = request.nextUrl.searchParams.get('client');
-    const supabase = await createClient();
+
+    const supabase = await createClient()
 
     const { error, data: widget } = await supabase
       .from('widgets')
       .select(
         ` *,
-          widget_channels(*, channels!inner(channel_name, channel_logo_url))
-        `
+            widget_channels(*, channels!inner(channel_name, channel_logo_url))
+          `
       )
       .eq('client_id', clientId)
       .maybeSingle();
 
-    const responseHeaders = new Headers({
-      ...corsHeaders, // Include CORS headers
-      "Content-Type": "application/json", // Ensure JSON response type
-    });
-
     if (!error) {
-      return new Response(JSON.stringify(widget), {
-        status: 200,
-        headers: responseHeaders,
-      });
+      return NextResponse.json(widget, { status: 200 });
     } else {
-      return new Response(JSON.stringify({ error }), {
-        status: 400,
-        headers: responseHeaders,
-      });
+      return NextResponse.json({ ...error }, { status: 400 });
     }
+
+
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
-    return new Response(
-      JSON.stringify({ error: errorMessage }),
-      {
-        status: 500,
-        headers: corsHeaders,
-      }
+    return NextResponse.json(
+      { error: errorMessage },
+      { status: 500 }
     );
   }
 }
+
+// You can also handle POST, PUT, DELETE, etc.
